b16ea4c8c18d4d3bf255015a11c3bd00cb9f9a1c
Mateus Campos <mateus.camphos2000@gmail.com>
2025-05-21 19:49:24 -0300

    Create cart_items table and add cart abandonment fields.

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_02_192736) do
+ActiveRecord::Schema[7.1].define(version: 2025_05_21_165511) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cart_items", force: :cascade do |t|
+    t.bigint "cart_id", null: false
+    t.bigint "product_id", null: false
+    t.integer "quantity", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["cart_id", "product_id"], name: "index_cart_items_on_cart_id_and_product_id", unique: true
+    t.index ["cart_id"], name: "index_cart_items_on_cart_id"
+    t.index ["product_id"], name: "index_cart_items_on_product_id"
+  end
 
   create_table "carts", force: :cascade do |t|
     t.decimal "total_price", precision: 17, scale: 2
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "abandoned", default: false
+    t.datetime "last_interaction_at"
   end
 
   create_table "products", force: :cascade do |t|
@@ -27,4 +40,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_02_192736) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "cart_items", "carts"
+  add_foreign_key "cart_items", "products"
 end
