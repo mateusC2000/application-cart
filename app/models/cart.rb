ab76aad76f805fa830c678930fd6ea9606e9c4b1
Mateus Campos <mateus.camphos2000@gmail.com>
2025-05-21 19:46:37 -0300

    Adds methods to mark and remove abandoned cart.

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,5 +1,19 @@
 class Cart < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
+
   validates_numericality_of :total_price, greater_than_or_equal_to: 0
 
-  # TODO: lógica para marcar o carrinho como abandonado e remover se abandonado
+  def mark_as_abandoned
+    update(abandoned: true) if inactive_for?(2.hours)
+  end
+
+  def remove_if_abandoned
+    destroy if abandoned? && last_interaction_at < 3.days.ago
+  end
+
+  private
+
+  def inactive_for?(duration)
+    last_interaction_at.present? && last_interaction_at < Time.current - duration
+  end
 end
